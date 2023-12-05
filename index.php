d2e92419cd2c2c7f4364e2231f9c0721d7337bf2
AIC Digital <53875525+PWPankaj@users.noreply.github.com>
2023-12-05 10:15:40 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <title>MY WEB APP 1</title>title>
+  </head>
+  <body>
+    <h1>Welcome to my 1st Web application</h1>
+    <?php
+    echo "<h1>THis App is running on AZure app services<h1>";
+    ?>
+  </body>
+</html>
